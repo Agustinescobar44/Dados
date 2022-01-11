6f8d4a52b23ab347b2105bb310db4996272d3092
Agustin Lautaro Escobar <agustinescobar@hotmail.com.ar>
2022-01-11 16:17:40 -0300

    Creacion del proyecto dados con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+
+  dadoIzq = "../assets/img/dice1.png";
+  dadoDere = "../assets/img/dice1.png";
+  numero1 = 1;
+  numero2 = 2;
+
+  rodarDados():void {
+    this.numero1 = Math.floor(Math.random() * 6) + 1;
+    this.numero2 = Math.floor(Math.random() * 6) + 1;
+    this.dadoIzq = `../assets/img/dice${this.numero1}.png`;
+    this.dadoDere = `../assets/img/dice${this.numero2}.png`;
+  }
+
 }
